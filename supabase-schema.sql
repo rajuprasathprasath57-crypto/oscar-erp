bb0948ee878f404b260b5bf80f4617ce1faa9bd4
Harish Harish <harishharish@Harishs-MacBook-Air.local>
2026-06-02 02:33:58 +0530

    fix: supabase-schema.sql with DELETE + RESTART sequences

--- supabase-schema.sql
+++ supabase-schema.sql
@@ -1,4 +1,10 @@
 -- Run this ENTIRE SQL in Supabase SQL Editor to reset sequences
+-- This will make new entries start from ID #1
 ALTER SEQUENCE enquiries_id_seq RESTART WITH 1;
 ALTER SEQUENCE productions_id_seq RESTART WITH 1;
 ALTER SEQUENCE dispatch_id_seq RESTART WITH 1;
+
+-- Also delete any remaining data just in case
+DELETE FROM dispatch;
+DELETE FROM productions;
+DELETE FROM enquiries;
